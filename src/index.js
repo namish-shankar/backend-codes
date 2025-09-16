8489b7be5a63a02b315734faf61df5f4e22edb22
Namish Shankar Srivastava <namishshankar15@gmail.com>
2025-09-16 21:00:25 +0530

    more updates in env and index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// requinre('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+dotenv.config(
+    {
+        path:"./.env"
+    }
+);
+// import mongoose from "mongoose";
+//  import { DB_NAME } from "./constants.js";  
+import connectDB from "./db/index.js";
+
+
+
+//APPROACH 1:
+// import express from "express";
+
+// const app=express()
+// (async () => {
+//     try {
+//         await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}', {);
+//         //console.log("Connected to MongoDB");
+//         app.on( "error encountered:",(error)=>{
+//             console.log("ERROR:",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log('app is listening on port ${process.env.PORT}:');
+//         })
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error); 
+//         throw error;
+//     }
+// })();
+
+
+//APPROACH 2:
+connectDB();
+
